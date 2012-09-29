25ace1ef21475df9f4dc2fd4949cc418ea1a6a26
jactheman <jactheman@gmail.com>
2012-09-29 09:26:28 -0400

    Added settings.

--- customhighlighter.py
+++ customhighlighter.py
@@ -4,9 +4,10 @@ import textwrap
 class HighlightOptions:
     def __init__(self):
         self.color_highlight = ""
-        self.words_included  = ""
+        self.words  = ""
 
-s = sublime.load_settings("customhighlight.sublime-settings")
+s = sublime.load_settings("customhighlighter.sublime-settings")
+
 def default_options():
     return HighlightOptions()
 
@@ -19,25 +20,20 @@ class HighlightCommand(sublime_plugin.TextCommand):
 		settings = self.view.settings()
 		opts = default_options()
 		all_reg = []
-		# sel = self.view.sel()[0]
-		# print(sel)
-		# # settings
-		opts.color_highlight = s.get("color_highlight")
-		opts.words_included  = s.get("words_included")
-		print("words")
-		print(opts.words_included)
+		# settings
+		opts.color_highlight = settings.get("color_highlight")
+		opts.words  = s.get("words")
 
-		for item in opts.words_included:
-			print("trying")
-			print(item)
-			sel = self.view.find_all(item, sublime.LITERAL)
-			all_reg.append(sel)
+
+		for item in opts.words:
+		    sel = self.view.find_all(item, sublime.LITERAL)
+		    all_reg.append(sel)
 
 		self.view.add_regions("WordHighlight", [item for sublist in all_reg for item in sublist], "circle", sublime.DRAW_EMPTY)
 
 
-		for reg in all_reg:
-			print(reg)
+		# for reg in all_reg:
+		# 	print(reg)
 			# self.view.add_regions("WordHighlight", reg, "circle", sublime.DRAW_EMPTY)
 
 
